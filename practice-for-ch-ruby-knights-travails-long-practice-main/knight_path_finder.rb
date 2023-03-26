500b87254600420df63e2d8114bfa3a85dd1ce77
root <somra45@gmail.com>
2023-03-26 16:57:15 -0400

    finish part 1

--- practice-for-ch-ruby-knights-travails-long-practice-main/knight_path_finder.rb
+++ practice-for-ch-ruby-knights-travails-long-practice-main/knight_path_finder.rb
@@ -1,54 +1,28 @@
+require "./poly_tree_node.rb"
+
 class KnightPathFinder
     attr_reader :root
     def initialize(start_pos, end_pos)
         @start_pos = start_pos 
         @end_pos = end_pos 
-        @root = PolyTreeNode.root_node(start_pos)
-        @board = Array.new(8) {Array.new(8), '_'}
+        @root = PolyTreeNode.new(start_pos)
+        @board = Array.new(8) {Array.new(8, '_')}
         @considered_positions = [start_pos]
+        build_move_tree(@root)
     end
 
     def build_move_tree(root)
-        start = root.initial_position
-        possible_moves = self.new_move_positions(start)
-        queue = []
-        possible_moves.each do |move|
-            new_node = PolyTreeNode.new(move)
-            root.children << new_node
-            queue << new_node
-            if queue.shift.initial_position == @end_pos
-                return
-            else
-                new_moves = new_node.new_move_positions(move)
-                new_moves.each do |move1|
-                    child_node = PolyTreeNode.new(move1)
-                    if queue.shift.initial_position == @end_pos
-                        return
-                    else
-                        new_node.children << child_node
-                        queue += new_node.children
-                    end
-                end
+        queue = [root]
+        until queue.empty?
+            current_node = queue.shift
+            possible_moves = new_move_positions(current_node.value)
+            possible_moves.each do |move|
+                new_node = PolyTreeNode.new(move)
+                current_node.add_child(new_node)
+                queue << new_node
+                @considered_positions << new_node.value
             end
-        end 
-
-
-    end
-
-    def build_move_tree_2(root)
-        start = root.initial_position
-        possible_moves = self.new_move_positions(start)
-
-        possible_moves.each do |move|
-            new_node = PolyTreeNode.new(move)
-            root.children << new_node
-            new_node.parent = root 
-            return new_node if new_node.initial_position == @end_pos
         end
-        root.children.each do |child|
-            result = self.build_move_tree_2(child)
-            return result if !result.nil?
-        end 
     end
     
     def self.valid_moves(start_pos)
@@ -76,3 +50,4 @@ class KnightPathFinder
         all_pos 
     end
 end
+
--- practice-for-ch-ruby-knights-travails-long-practice-main/poly_tree_node.rb
+++ practice-for-ch-ruby-knights-travails-long-practice-main/poly_tree_node.rb
@@ -1,13 +1,62 @@
 class PolyTreeNode
-    def self.root_node(position)
-        PolyTreeNode.new(position)
-    end
-    
-    attr_reader :initial_position, :children, :parent 
-    
-    def initialize(initial_position)
-        @initial_position = initial_position
+    attr_reader :parent, :children, :value
+
+    def initialize(value = nil)
+        @value = value
+        @parent = nil
         @children = []
-        @parent = []
     end
+
+    def parent=(parent_node)
+        if self.parent != nil 
+            self.parent.children.reject! {|ele| ele == self}
+        end
+        @parent = parent_node
+        if parent_node != nil &&  !parent_node.children.include?(self)
+            parent_node.children << self 
+            
+        end
+
+    end
+
+    def add_child(node_child)
+        self.children << node_child 
+        node_child.parent = self 
+    end
+
+    def remove_child(self_child)
+        if self.children.include?(self_child)
+            self_child.parent = nil 
+            self.children.reject! { |el| el == self_child }
+        else
+            raise "Node is not a child."
+        end
+    end 
+
+    def dfs(target)
+        return self if self.value == target
+
+        self.children.each do |child|
+            result = child.dfs(target)
+            if result != nil
+                return result
+            end
+        end
+
+        nil
+    end
+
+    def bfs(target)
+        return self if self.value == target
+        line = self.children
+        until line.empty?
+            removed_child = line.shift
+            if removed_child.value == target
+                return removed_child
+            else
+                line += removed_child.children
+            end
+        end
+        nil
+    end 
 end
